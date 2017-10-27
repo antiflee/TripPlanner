a12774d3ef1e02727db73d576b70c86d6795927e
YL-Office-PC <antiflee@gmail.com>
2017-10-27 11:02:10 -0400

    use environ var as SECRET_KEY in settings.py

--- tripplanner/settings/local.py
+++ tripplanner/settings/local.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'htx5(4yceoy0z$xal%0r(^_iqqzg+jrpz&n1*-f5_qf&^07_54'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
